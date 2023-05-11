f1eb614141e3c1566ef63e5747d75d30a3c85194
mateoleont <mateolt505@gmail.com>
2023-05-10 22:32:39 -0500

    project

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+const getCharById = (res, id) =>
+{
+    axios
+    .get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(response => response.data)
+    .then(({name, gender, species, origin, image, status}) =>
+    {
+        const character = 
+        {
+            id, 
+            name, 
+            gender, 
+            species, 
+            origin, 
+            image, 
+            status
+        }
+        return res
+                   .writeHead(200, {'Content-Type': 'applycation/json'})
+                   .end(JSON.stringify(character))
+    })
+    .catch(error =>
+        {
+            return res
+                      .writeHead(500, {'Content-Type': 'text/plain'})
+                      .end(error.message)
+        }
+    )
+}
+module.exports = 
+{
+    getCharById
+};
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,33 +1,13 @@
 const http = require('http');
-const data = require('./utils/data.js')
+const { getCharById } = require('./controllers/getCharById.js');
 
 http.createServer((req, res) =>
 {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    const { url } = req;
 
-    if (url.includes("/rickandmorty/character")) 
+    if (req.url.includes("/rickandmorty/character")) 
     {
-        const divUrl = url.split('/');
-        const id = parseInt(divUrl[divUrl.length - 1]);
-        console.log(id);
-        //-----------------------------------
-        if (id > 5 || id < 1) 
-        {
-            res.writeHead(400, "content-type", "text/plain");
-            res.end('El id debe estar entre 1 y 5');    
-        }
-        //-----------------------------------
-        data.map((char) =>
-        {
-            if (char.id === id) 
-            {
-                const character = char.id;    
-                res.writeHead(200, {'content-type': 'application/json'});
-                return res.end(JSON.stringify(char));
-            }
-        })
-        
-
+        const id = req.url.split('/').at(-1);
+        getCharById(res, +id);
     }
 }).listen(3001, 'localhost');
